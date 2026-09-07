f384752609516437f5141f8c972b01333d31dc7f
RelaxUniver <titorelaxuniver@gmail.com>
2026-09-07 12:56:04 -0400

    Implementando las clases y metodos insertar, obtener, longitud vacia... Ya habiendo creado la Class Tarea, la Interface TDALista, cree un enum para la Prioridad de Tarea y creando la Class ListaSecuencial... Cosas q me faltan: Implementar el metodo eliminar

---
+++ tdalistasecuencial/src/main/java/com/yaisel/ListaSecuencial.java
@@ -0,0 +1,53 @@
+package com.yaisel;
+
+public class ListaSecuencial<T> implements TDALista<T> {
+
+    private Object[] array;
+    private int longitud = 0;
+    private int capacidad;
+
+    public ListaSecuencial(int capacidad) {
+        this.capacidad = capacidad;
+        this.array = new Object[capacidad];
+    }
+
+    @Override
+    public void insertar(T object, int i) {
+        if (((0 <= i) && (i <= this.longitud)) && (this.longitud < this.capacidad)) {
+            if (i < this.longitud) {
+                for (int j = longitud; i<j; j--) {
+                        array[j] = array[j-1];
+                }
+                array[i] = object;
+            } else {
+                array[i] = object;
+            }
+            longitud++;
+        }
+    }
+
+    @Override
+    public T obtener(int i) {
+        if (((0 <= i) && (i < this.longitud))) {
+            return (T) array[i];
+        }
+        throw new IndexOutOfBoundsException("Posición Invalida");
+    }
+
+    @Override
+    public void eliminar(int i) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'eliminar'");
+    }
+
+    @Override
+    public int longitud() {
+        return this.longitud;
+    }
+
+    @Override
+    public boolean vacia() {
+        return longitud == 0;
+    }
+
+}
--- tdalistasecuencial/src/main/java/com/yaisel/TDALista.java
+++ tdalistasecuencial/src/main/java/com/yaisel/TDALista.java
@@ -2,10 +2,10 @@ package com.yaisel;
 
 public interface TDALista<T> {
 
-    public void insertar(Object object, int i);
-    public Object obtener(int i);
+    public void insertar(T object, int i);
+    public T obtener(int i);
     public void eliminar(int i);
     public int longitud();
-    public boolean vacía();
+    public boolean vacia();
 
 }
